a16927448565921bcb3ffa53743c8556b2ea1e79
Samuel <samuelcalderon4343@gmail.com>
2023-11-05 21:29:18 +0100

    middleware de errores arreglado

--- practicasVoluntarias/app.js
+++ practicasVoluntarias/app.js
@@ -20,11 +20,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/users', usersRouter);
 
 //Ruta principal
-app.get("/", function(request, response) {
+app.get("/", function(request, response,next) {
   response.status(200);
   daoDestino.buscarTodosDestinos((err, dest) => {
     if (err) {
-        console.error("Error al buscar el usuario:", err);
+      next(err);
     } else {
         dest.forEach(d => {
           d.imagen = JSON.parse(d.imagen); //Parseamos bien el JSON
@@ -35,26 +35,45 @@ app.get("/", function(request, response) {
 });
 
 //Parametrico para cada posible destino
-app.get("/:dest", function(request, response){
+app.get("/:dest", function(request, response,next){
   const destino = request.params.dest;
   response.status(200);
   daoDestino.buscarDestino(destino, (err, dest) => {
     if (err) {
-      console.error("Error al buscar el usuario:", err);
-    } else {
-      //Parseamos el JSON para que este bien
-      dest.imagen = JSON.parse(dest.imagen);
-      dest.descripcion = JSON.parse(dest.descripcion);
-      response.render("destino", {dest: dest});
+      next(err);
+    } else {  
+      //el destion URL no existe
+      if(dest === undefined)
+        next();
+      else{
+        //Parseamos el JSON para que este bien
+        dest.imagen = JSON.parse(dest.imagen);
+        dest.descripcion = JSON.parse(dest.descripcion);
+        response.render("destino", {dest: dest});
+      }
     }
   });
 });
 
-//middleware de errores NOT FOUND URL
+// Middleware para manejar rutas no encontradas
 app.use((request, response, next) => {
-  response.status(404);
-  response.render("error", { url: request.url });
+  const error = new Error("Página no encontrada");
+  error.status = 404;
+  next(error);
 });
+
+
+//middleware de errores 
+app.use((error, request, response, next) => {
+  // Código 500: Internal server error
+  response.status(error.status || 500);
+  response.render("error", {
+    status : error.status,
+    mensaje: error.message,
+    pila: error.stack
+  });
+});
+
 
 
 app.listen(3000, function(err) {

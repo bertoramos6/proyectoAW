147c97c25fa7bebfe7fac1823a25318193685e06
Samuel <samuelcalderon4343@gmail.com>
2023-11-05 20:34:02 +0100

    DAOReserva

---
+++ practicasVoluntarias/database/DAOReserva.js
@@ -0,0 +1,63 @@
+"use strict";
+const db = require("./configuration"); // Info de la conexion con la BBDD
+
+class DAOReserva {
+    pool;
+
+    constructor() {
+        this.pool = db.getPool();
+    }
+
+    insertarReserva(reserva,callback){
+        this.pool.getConnection((err, connection) => {
+            if (err) {
+                callback(err)
+            } else {
+                connection.query(
+                    `INSERT INTO reservas destino_id, nombre_cliente, correo_cliente, fecha_inicio, fecha_fin
+                    VALUES (?,?,?,?,?)`,
+                    [reserva.dest], [reserva.nombre], [reserva.correo], [reserva.inicio], [reserva.fin],
+                    function(err, rows){
+                        connection.release();
+                        if(err){
+                            callback(err,null);
+                        }
+                        else{
+                            callback(null,rows.insertId) //devolvemos el id devulto por la BD
+                        }
+                    }
+                )
+            }
+        });
+    }
+
+    //TODO Nos puede servir en un futuro
+    buscarPorNombre(str, callback){
+        this.pool.getConnection((err, connection) => {
+            if (err) {
+                callback(err)
+            } else {
+                connection.query(
+                    `SELECT * FROM reservas WHERE nombre_cliente = ?`,
+                    [str],
+                    function(err, rows){
+                        connection.release();
+                        if(err){
+                            callback(err, null);
+                        }
+                        else{
+                            let data = JSON.parse(JSON.stringify(rows));
+                            callback(null, data); //devolvemos todas las reservas del cliente
+                        }
+                    }
+                )
+            }
+        });
+    }
+
+    terminarConexion(callback) {
+        this.pool.end();
+    }
+}
+
+module.exports = DAOReserva;
